7ac10d652605bf52af82b0ef2c319311bfa468e2
github username <github email address>
2017-12-19 00:08:42 -0500

    started seed data for tax brackets, states half done

--- x_proj/db/migrate/20171215195639_create_federal_brackets.rb
+++ x_proj/db/migrate/20171215195639_create_federal_brackets.rb
@@ -1,6 +1,9 @@
 class CreateFederalBrackets < ActiveRecord::Migration[5.1]
   def change
     create_table :federal_brackets do |t|
+      t.float :new_rate
+      t.float :bracket_max
+      t.float :initial_tax
     end
   end
 end
--- x_proj/db/seeds.rb
+++ x_proj/db/seeds.rb
@@ -5,3 +5,386 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+[{'marital_status': 'single',
+  [
+    {"state":"AL",
+      [
+        {"new_rate":0.02,"bracket_max":0,"initial_tax":null},
+        {"new_rate":0.04,"bracket_max":500,"initial_tax":10},
+        {"new_rate":0.05,"bracket_max":3.000,"initial_tax":135}
+      ]
+    },
+
+    {"state":"AK",
+      [
+        {"new_rate":0.00,"bracket_max":null,"initial_tax":null}
+      ]
+    },
+
+    {"state":"AZ",
+      [
+        {"new_rate":0.03,"bracket_max":0,"initial_tax":null},
+        {"new_rate":0.03,"bracket_max":10.179,"initial_tax":263.6361},
+        {"new_rate":0.03,"bracket_max":25.445,"initial_tax":776.5737},
+        {"new_rate":0.04,"bracket_max":50.890,"initial_tax":1855.4417},
+        {"new_rate":0.05,"bracket_max":152.668,"initial_tax":6476.1629}
+      ]
+    },
+
+    {"state":"AR",
+      [
+        {"new_rate":0.01,"bracket_max":0,"initial_tax":null},
+        {"new_rate":0.03,"bracket_max":4.299,"initial_tax":38.691},
+        {"new_rate":0.04,"bracket_max":8.500,"initial_tax":185.726},
+        {"new_rate":0.05,"bracket_max":12.699,"initial_tax":374.681},
+        {"new_rate":0.06,"bracket_max":21.199,"initial_tax":884.681},
+        {"new_rate":0.07,"bracket_max":35.100,"initial_tax":1843.85}
+      ]
+    },
+
+
+    {"state":"CA",
+      [
+        {"new_rate":0.01,"bracket_max":0,"initial_tax":null},
+        {"new_rate":0.02,"bracket_max":8.015,"initial_tax":80.15},
+        {"new_rate":0.04,"bracket_max":19.001,"initial_tax":519.59},
+        {"new_rate":0.06,"bracket_max":29.989,"initial_tax":1178.87},
+        {"new_rate":0.08,"bracket_max":41.629,"initial_tax":2110.07},
+        {"new_rate":0.09,"bracket_max":52.612,"initial_tax":3131.489},
+        {"new_rate":0.10,"bracket_max":268.750,"initial_tax":25393.703},
+        {"new_rate":0.11,"bracket_max":322.499,"initial_tax":31467.34},
+        {"new_rate":0.12,"bracket_max":537.498,"initial_tax":57912.217},
+        {"new_rate":0.13,"bracket_max":1,000,000,"initial_tax":119424.983}
+      ]
+    },
+
+
+    {"state":"CO",
+      [{"new_rate":0.05,"bracket_max":null,"initial_tax":null}
+      ]
+    },
+
+
+
+    {"state":"CT",
+      [
+        {"new_rate":0.03,"bracket_max":0,"initial_tax":null},
+        {"new_rate":0.05,"bracket_max":10.000,"initial_tax":300},
+        {"new_rate":0.06,"bracket_max":50.000,"initial_tax":2500},
+        {"new_rate":0.06,"bracket_max":100.000,"initial_tax":5500},
+        {"new_rate":0.07,"bracket_max":200.000,"initial_tax":12000},
+        {"new_rate":0.07,"bracket_max":250.000,"initial_tax":15450},
+        {"new_rate":0.07,"bracket_max":500.000,"initial_tax":32925}
+      ]
+    },
+
+    {"state":"DE",
+      [
+        {"new_rate":0.02,"bracket_max":2.000,"initial_tax":null},
+        {"new_rate":0.04,"bracket_max":5.000,"initial_tax":110},
+        {"new_rate":0.05,"bracket_max":10.000,"initial_tax":350},
+        {"new_rate":0.05,"bracket_max":20.000,"initial_tax":870},
+        {"new_rate":0.06,"bracket_max":25.000,"initial_tax":1147.5},
+        {"new_rate":0.07,"bracket_max":60.000,"initial_tax":3457.5}
+      ]
+    },
+
+    {"state":"FL",
+      [
+        {"new_rate":0,"bracket_max":null,"initial_tax":null}
+      ]
+    },
+
+    {"state":"GA",
+      [
+        {"new_rate":0.01,"bracket_max":0,"initial_tax":null},
+        {"new_rate":0.02,"bracket_max":750,"initial_tax":7.5},
+        {"new_rate":0.03,"bracket_max":2.250,"initial_tax":52.5},
+        {"new_rate":0.04,"bracket_max":3.750,"initial_tax":112.5},
+        {"new_rate":0.05,"bracket_max":5.250,"initial_tax":187.5},
+        {"new_rate":0.06,"bracket_max":7.000,"initial_tax":292.5}
+      ]
+    },
+
+    {"state":"HI",
+      [
+        {"new_rate":0.01,"bracket_max":0,"initial_tax":null},
+        {"new_rate":0.03,"bracket_max":2.400,"initial_tax":33.6},
+        {"new_rate":0.06,"bracket_max":4.800,"initial_tax":165.6},
+        {"new_rate":0.06,"bracket_max":9.600,"initial_tax":472.8},
+        {"new_rate":0.07,"bracket_max":14.400,"initial_tax":799.2},
+        {"new_rate":0.07,"bracket_max":19.200,"initial_tax":1144.8},
+        {"new_rate":0.08,"bracket_max":24.000,"initial_tax":1509.6},
+        {"new_rate":0.08,"bracket_max":36.000,"initial_tax":2457.6},
+        {"new_rate":0.08,"bracket_max":48.000,"initial_tax":3447.6}
+      ]
+    },
+
+    {"state":"ID",
+      [
+        {"new_rate":0.02,"bracket_max":0,"initial_tax":null},
+        {"new_rate":0.04,"bracket_max":1.454,"initial_tax":23.264},
+        {"new_rate":0.04,"bracket_max":2.908,"initial_tax":82.878},
+        {"new_rate":0.05,"bracket_max":4.362,"initial_tax":157.032},
+        {"new_rate":0.06,"bracket_max":5.816,"initial_tax":245.726},
+        {"new_rate":0.07,"bracket_max":7.270,"initial_tax":348.96},
+        {"new_rate":0.07,"bracket_max":10.905,"initial_tax":617.95}
+      ]
+    },
+
+    {"state":"IL",
+      [
+        {"new_rate":0.05,"bracket_max":null,"initial_tax":null}
+      ]
+    },
+
+
+    {"state":"IN",
+      [
+        {"new_rate":0.03,"bracket_max":null,"initial_tax":null}
+      ]
+    },
+
+
+    {"state":"IA",
+      [
+        {"new_rate":0.00,"bracket_max":0,"initial_tax":null},
+        {"new_rate":0.01,"bracket_max":1.573,"initial_tax":5.6628},
+        {"new_rate":0.02,"bracket_max":3.146,"initial_tax":43.8867},
+        {"new_rate":0.05,"bracket_max":6.292,"initial_tax":185.4567},
+        {"new_rate":0.06,"bracket_max":14.157,"initial_tax":666.7947},
+        {"new_rate":0.06,"bracket_max":23.595,"initial_tax":1278.3771},
+        {"new_rate":0.07,"bracket_max":31.460,"initial_tax":1813.1971},
+        {"new_rate":0.08,"bracket_max":47.190,"initial_tax":3059.0131},
+        {"new_rate":0.09,"bracket_max":70.785,"initial_tax":5177.8441}
+      ]
+    },
+
+    {"state":"KS",
+      [
+        {"new_rate":0.03,"bracket_max":0,"initial_tax":null},
+        {"new_rate":0.05,"bracket_max":15.000,"initial_tax":435},
+        {"new_rate":0.05,"bracket_max":30.000,"initial_tax":1215}
+      ]
+    },
+
+    {"state":"KY",
+      [
+        {"new_rate":0.02,"bracket_max":0,"initial_tax":null},
+        {"new_rate":0.03,"bracket_max":3.000,"initial_tax":60},
+        {"new_rate":0.04,"bracket_max":4.000,"initial_tax":100},
+        {"new_rate":0.05,"bracket_max":5.000,"initial_tax":150},
+        {"new_rate":0.06,"bracket_max":8.000,"initial_tax":324},
+        {"new_rate":0.06,"bracket_max":75.000,"initial_tax":4344}
+      ]
+    },
+
+    {"state":"LA",
+      [
+        {"new_rate":0.02,"bracket_max":0,"initial_tax":null},
+        {"new_rate":0.04,"bracket_max":12.500,"initial_tax":250},
+        {"new_rate":0.06,"bracket_max":50.000,"initial_tax":2500}
+      ]
+    },
+
+    {"state":"ME",
+      [
+        {"new_rate":0.06,"bracket_max":0,"initial_tax":null},
+        {"new_rate":0.07,"bracket_max":21.050,"initial_tax":1220.9},
+        {"new_rate":0.07,"bracket_max":50.000,"initial_tax":3290.825}
+      ]
+    },
+
+    {"state":"MD",
+        [
+          {"new_rate":0.02,"bracket_max":0,"initial_tax":null},
+          {"new_rate":0.03,"bracket_max":1.000,"initial_tax":20},
+          {"new_rate":0.04,"bracket_max":2.000,"initial_tax":60},
+          {"new_rate":0.05,"bracket_max":3.000,"initial_tax":107.5},
+          {"new_rate":0.05,"bracket_max":100.000,"initial_tax":4957.5},
+          {"new_rate":0.05,"bracket_max":125.000,"initial_tax":6270},
+          {"new_rate":0.06,"bracket_max":150.000,"initial_tax":7645},
+          {"new_rate":0.06,"bracket_max":250.000,"initial_tax":13395}
+        ]
+      },
+
+    {"state":"MA",
+      [
+        {"new_rate":0.05,"bracket_max":null,"initial_tax":null}
+      ]
+    },
+
+    {"state":"MI",
+      [
+        {"new_rate":0.04,"bracket_max":null,"initial_tax":null}
+      ]
+    },
+
+    {"state":"MN",
+      [
+        {"new_rate":0.05,"bracket_max":0,"initial_tax":null},
+        {"new_rate":0.07,"bracket_max":25.390,"initial_tax":1358.365},
+        {"new_rate":0.08,"bracket_max":83.400,"initial_tax":5912.15},
+        {"new_rate":0.10,"bracket_max":156.911,"initial_tax":13152.9835}
+      ]
+    },
+
+    {"state":"MS",
+      [
+        {"new_rate":0.03,"bracket_max":0,"initial_tax":null},
+        {"new_rate":0.04,"bracket_max":5.000,"initial_tax":150},
+        {"new_rate":0.05,"bracket_max":10.000,"initial_tax":400}
+      ]
+    },
+
+    {"state":"MO",
+      [
+        {"new_rate":0.02,"bracket_max":0,"initial_tax":null},
+        {"new_rate":0.02,"bracket_max":1.008,"initial_tax":15.12},
+        {"new_rate":0.03,"bracket_max":2.016,"initial_tax":40.32},
+        {"new_rate":0.03,"bracket_max":3.024,"initial_tax":70.56},
+        {"new_rate":0.04,"bracket_max":4.032,"initial_tax":105.84},
+        {"new_rate":0.04,"bracket_max":5.040,"initial_tax":146.16},
+        {"new_rate":0.05,"bracket_max":6.048,"initial_tax":191.52},
+        {"new_rate":0.05,"bracket_max":7.056,"initial_tax":241.92},
+        {"new_rate":0.06,"bracket_max":8.064,"initial_tax":297.36},
+        {"new_rate":0.06,"bracket_max":9.072,"initial_tax":357.84}
+      ]
+    },
+
+    {"state":"MT",
+      [
+        {"new_rate":0.01,"bracket_max":0,"initial_tax":null},
+        {"new_rate":0.02,"bracket_max":2.900,"initial_tax":29},
+        {"new_rate":0.03,"bracket_max":5.200,"initial_tax":98},
+        {"new_rate":0.04,"bracket_max":7.900,"initial_tax":206},
+        {"new_rate":0.05,"bracket_max":10.600,"initial_tax":341},
+        {"new_rate":0.06,"bracket_max":13.600,"initial_tax":521},
+        {"new_rate":0.07,"bracket_max":17.600,"initial_tax":797}
+      ]
+    }
+
+    {"state":"NE",
+      [
+        {"new_rate":0.02,"bracket_max":0,"initial_tax":null},
+        {"new_rate":0.04,"bracket_max":3.090,"initial_tax":76.014},
+        {"new_rate":0.05,"bracket_max":18.510,"initial_tax":848.556},
+        {"new_rate":0.07,"bracket_max":29.830,"initial_tax":1622.844}
+      ]
+    },
+
+    {"state":"NV",
+      [
+        {"new_rate":0,"bracket_max":null,"initial_tax":null}
+      ]
+    },
+
+    {"state":"NH","new_rate":0.05,"bracket_max":0,"initial_tax":null},
+
+    {"state":"NJ","new_rate":0.01,"bracket_max":0,"initial_tax":null},
+    {"new_rate":0.02,"bracket_max":20.000,"initial_tax":280},
+    {"new_rate":0.04,"bracket_max":35.000,"initial_tax":805},
+    {"new_rate":0.055,"bracket_max":40.000,"initial_tax":1081.25},
+    {"new_rate":0.06,"bracket_max":75.000,"initial_tax":3310.75},
+    {"new_rate":0.09,"bracket_max":500.000,"initial_tax":41433.25},
+
+
+    {"state":"NM","new_rate":0.02,"bracket_max":0,"initial_tax":null},
+    {"new_rate":0.03,"bracket_max":5.500,"initial_tax":93.5},
+    {"new_rate":0.05,"bracket_max":11.000,"initial_tax":352},
+    {"new_rate":0.05,"bracket_max":16.000,"initial_tax":597},
+
+    {"state":"NY","new_rate":0.04,"bracket_max":0,"initial_tax":null},
+    {"new_rate":0.05,"bracket_max":8.500,"initial_tax":340},
+    {"new_rate":0.05,"bracket_max":11.700,"initial_tax":508},
+    {"new_rate":0.06,"bracket_max":13.900,"initial_tax":637.8},
+    {"new_rate":0.06,"bracket_max":21.400,"initial_tax":1121.55},
+    {"new_rate":0.07,"bracket_max":80.650,"initial_tax":5061.675},
+    {"new_rate":0.07,"bracket_max":215.400,"initial_tax":14292.05},
+    {"new_rate":0.09,"bracket_max":1,077,550,"initial_tax":90333.68},
+
+    {"state":"NC","new_rate":0.055,"bracket_max":0,"initial_tax":null},
+
+    {"state":"ND","new_rate":0.01,"bracket_max":0,"initial_tax":null},
+    {"new_rate":0.02,"bracket_max":37.950,"initial_tax":417.45},
+    {"new_rate":0.02,"bracket_max":91.900,"initial_tax":1642.115},
+    {"new_rate":0.03,"bracket_max":191.650,"initial_tax":4275.515},
+    {"new_rate":0.03,"bracket_max":416.700,"initial_tax":10801.965},
+
+    {"state":"OH","new_rate":0.005,"bracket_max":0,"initial_tax":null},
+    {"new_rate":0.010,"bracket_max":5.250,"initial_tax":25.9875},
+    {"new_rate":0.020,"bracket_max":10.500,"initial_tax":129.9375},
+    {"new_rate":0.025,"bracket_max":15.800,"initial_tax":261.1655},
+    {"new_rate":0.030,"bracket_max":21.100,"initial_tax":418.5225},
+    {"new_rate":0.035,"bracket_max":42.100,"initial_tax":1146.1725},
+    {"new_rate":0.040,"bracket_max":84.200,"initial_tax":2813.3325},
+    {"new_rate":0.046,"bracket_max":105.300,"initial_tax":3783.2995},
+    {"new_rate":0.050,"bracket_max":210.600,"initial_tax":9045.1405},
+
+    {"state":"OK","new_rate":0.01,"bracket_max":0,"initial_tax":null},
+    {"new_rate":0.01,"bracket_max":1.000,"initial_tax":5},
+    {"new_rate":0.02,"bracket_max":2.500,"initial_tax":35},
+    {"new_rate":0.03,"bracket_max":3.750,"initial_tax":72.5},
+    {"new_rate":0.04,"bracket_max":4.900,"initial_tax":118.5},
+    {"new_rate":0.05,"bracket_max":7.200,"initial_tax":233.5},
+
+    {"state":"OR","new_rate":0.05,"bracket_max":0,"initial_tax":null},
+    {"new_rate":0.07,"bracket_max":3.350,"initial_tax":167.5},
+    {"new_rate":0.09,"bracket_max":8.450,"initial_tax":626.5},
+    {"new_rate":0.10,"bracket_max":125.000,"initial_tax":12164.95},
+
+    {"state":"PA","new_rate":0.03,"bracket_max":0,"initial_tax":null},
+
+    {"state":"RI","new_rate":0.04,"bracket_max":0,"initial_tax":null},
+    {"new_rate":0.05,"bracket_max":61.300,"initial_tax":2298.75},
+    {"new_rate":0.06,"bracket_max":139.400,"initial_tax":6976.94},
+
+    {"state":"SC","new_rate":0.00,"bracket_max":0,"initial_tax":null},
+    {"new_rate":0.03,"bracket_max":2.930,"initial_tax":0},
+    {"new_rate":0.04,"bracket_max":5.860,"initial_tax":117.2},
+    {"new_rate":0.05,"bracket_max":8.790,"initial_tax":263.7},
+    {"new_rate":0.06,"bracket_max":11.720,"initial_tax":439.5},
+    {"new_rate":0.07,"bracket_max":14.650,"initial_tax":644.6},
+
+    {"state":"SD","new_rate":0.00,"bracket_max":null,"initial_tax":null},
+
+    {"state":"TN","new_rate":0.05,"bracket_max":0,"initial_tax":null},
+
+    {"state":"TX","new_rate":0.00,"bracket_max":null,"initial_tax":null},
+
+    {"state":"UT","new_rate":0.05,"bracket_max":0,"initial_tax":null},
+
+    {"state":"Vt.","new_rate":0.04,"bracket_max":0,"initial_tax":null},
+    {"state":" (e, p)","new_rate":0.07,"bracket_max":37.950,"initial_tax":1347.225},
+    {"new_rate":0.08,"bracket_max":91.900,"initial_tax":5555.325},
+    {"new_rate":0.09,"bracket_max":191.650,"initial_tax":14333.325},
+    {"new_rate":0.09,"bracket_max":416.700,"initial_tax":34475.3},
+
+    {"state":"Va.","new_rate":0.02,"bracket_max":0,"initial_tax":null},
+    {"new_rate":0.03,"bracket_max":3.000,"initial_tax":60},
+    {"new_rate":0.05,"bracket_max":5.000,"initial_tax":160},
+    {"new_rate":0.06,"bracket_max":17.000,"initial_tax":850},
+
+    {"state":"WA","new_rate":0.00,"bracket_max":null,"initial_tax":null},
+
+    {"state":"WV","new_rate":0.03,"bracket_max":0,"initial_tax":null},
+    {"new_rate":0.04,"bracket_max":10.000,"initial_tax":300},
+    {"new_rate":0.05,"bracket_max":25.000,"initial_tax":975},
+    {"new_rate":0.06,"bracket_max":40.000,"initial_tax":1875},
+    {"new_rate":0.07,"bracket_max":60.000,"initial_tax":3175},
+
+    {"state":"WI","new_rate":0.04,"bracket_max":0,"initial_tax":null},
+    {"new_rate":0.06,"bracket_max":11.230,"initial_tax":449.2},
+    {"new_rate":0.06,"bracket_max":22.470,"initial_tax":1153.948},
+    {"new_rate":0.08,"bracket_max":247.350,"initial_tax":18357.268},
+
+    {"state":"WY","new_rate":none,"bracket_max":0,"initial_tax":null},
+
+    {"state":"DC","new_rate":0.04,"bracket_max":0,"initial_tax":null},
+    {"new_rate":0.06,"bracket_max":10.000,"initial_tax":400},
+    {"new_rate":0.07,"bracket_max":40.000,"initial_tax":2350},
+    {"new_rate":0.09,"bracket_max":60.000,"initial_tax":4050},
+    {"new_rate":0.09,"bracket_max":350.000,"initial_tax":29425},
+    {"new_rate":0.09,"bracket_max":1,000,000,"initial_tax":87600}]
+    ]}
